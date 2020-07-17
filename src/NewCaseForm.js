95dc5a4bcb257497a5c5a64206f31f86fe97e158
CharliePayne1 <charliepayne1101@gmail.com>
2020-07-17 09:11:57 +0100

    upload photo functionality completed

--- src/NewCaseForm.js
+++ src/NewCaseForm.js
@@ -1,29 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './App.css';
 
 export default function NewCaseForm() {
+
+  const [image, setImage] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  const uploadImage = async e => {
+    const files = e.target.files
+    const data = new FormData()
+    data.append('file', files[0])
+    data.append("upload_preset", "triple_one")
+    setLoading(true)
+    const res = await fetch('https://api.cloudinary.com/v1_1/debtlnnlh/image/upload', {
+      method: 'POST', 
+      body: data
+    }
+    )
+    const file = await res.json()
+    setImage(file.secure_url)
+    setLoading(false)
+  }
+
     return (
       <>
         <h5>New Case</h5>
-        {/* <select>
-        <option  value="select">- Select Category -</option>
-            <option  value="Option 1">Option 1</option>
-            <option value="Option 2">Option 2</option>
-            <option value="Option 3">Option 3</option>
-            <option value="Option 4">Option 4</option>
-            <option value="Option 5">Option 5</option>
+        <select>
+              <option>- Select Category -</option>
+              <option>Option 1</option>
+              <option>Option 2</option>
+              <option>Option 3</option>
+              <option>Option 4</option>
+              <option>Option 5</option>
         </select>
         <br></br>
         <br></br>
-        <input type="text" value="Symptoms"placeholder="Symptoms"></input>
+        <input type="text" name="Symptoms" placeholder="Symptoms"/>
         <br></br>
         <br></br>
-        <input type="text" value="Image"placeholder="Image"></input>
+        <input type="file" name="file" placeholder="Upload Image" onChange={uploadImage}/>
+        {loading ? (<h5>Loading...</h5>) : (<img src={image} className="image" alt="" />)}
         <br></br>
         <br></br>
-        <input type="text" value="Email"placeholder="Email"></input>
+        <input type="text" name="Email" placeholder="Email"/>
         <br></br>
         <br></br>
-        <button>Submit</button> */}
+        <button>Submit</button>
       </>
     );
   }
